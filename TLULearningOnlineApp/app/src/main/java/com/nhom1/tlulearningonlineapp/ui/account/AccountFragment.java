f28eab6cea3ba4ab03159e141bb6556828079550
Nhật Anh <pna120104@gmail.com>
2025-06-18 04:43:01 +0700

    update

--- TLULearningOnlineApp/app/src/main/java/com/nhom1/tlulearningonlineapp/ui/account/AccountFragment.java
+++ TLULearningOnlineApp/app/src/main/java/com/nhom1/tlulearningonlineapp/ui/account/AccountFragment.java
@@ -1,11 +1,17 @@
 package com.nhom1.tlulearningonlineapp.ui.account;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import com.nhom1.tlulearningonlineapp.R;
 
@@ -18,5 +24,23 @@ public class AccountFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_account, container, false);
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button btnGo = view.findViewById(R.id.btnCreateAccount);
+        btnGo.setOnClickListener(v -> {
+            NavController navController = Navigation.findNavController(view);
+            navController.navigate(R.id.action_accountFragment_to_addAccountMethodFragment);
+            Log.d("MyDebug", "Button clicked");
+        });
+
+        Button btnList = view.findViewById(R.id.btnListAccount);
+        btnList.setOnClickListener(v -> {
+            NavController navController = Navigation.findNavController(view);
+            navController.navigate(R.id.action_accountFragment_to_listAccountFragment);
+            Log.d("MyDebug", "Button clicked");
+        });
     }
 }
